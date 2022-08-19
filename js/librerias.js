a22c8f07a5d23c4214af56d42586d2aff5e7434c
Tomas Diuorno <tom.diuorno99@gmail.com>
2022-08-18 23:14:08 -0300

    cambios añadidos

---
+++ js/librerias.js
@@ -0,0 +1,7 @@
+$("body").vegas({
+    slides: [
+        { src: "img/portadacontacto.jpg" },
+        { src: "img/miembrosportada.jpg" },
+        { src: "img/portadadiscografia.png" }
+    ]
+});
---
+++ js/stock.js
@@ -0,0 +1,12 @@
+// stock
+let stockProductos = [
+    {id: 1, nombre: "Remera 1", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1250, talle: "L", img: "./img/remera.jpg"},
+    {id: 2, nombre: "Remera 2", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1200, talle: "L", img: "./img/remera.jpg"},
+    {id: 3, nombre: "Remera 3", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1270, talle: "L", img: "./img/remera.jpg"},
+    {id: 4, nombre: "Remera 4", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1230, talle: "L", img: "./img/remera.jpg"},
+    {id: 5, nombre: "Remera 5", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1300, talle: "L", img: "./img/remera.jpg"},
+    {id: 6, nombre: "Remera 6", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1290, talle: "L", img: "./img/remera.jpg"},
+    {id: 7, nombre: "Remera 7", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1240, talle: "L", img: "./img/remera.jpg"},
+    {id: 8, nombre: "Remera 8", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1310, talle: "L", img: "./img/remera.jpg"},
+    {id: 9, nombre: "Remera 9", tipo: "remera", cantidad: 1, desc: "Una remera para ROCKEARLA", precio: 1190, talle: "L", img: "./img/remera.jpg"},
+];
